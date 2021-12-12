12562e12551523b60d193f78fd696913ab64002c
SvitlanaYeremenko <94764748+SvitlanaYeremenko@users.noreply.github.com>
2021-12-12 13:46:34 +0100

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,27 @@
+public class ATM {
+    public int countBanknotes(int sum) {
+
+        int[] noteValues= {500,200,100,50,20,10,5,2,1};
+        int value = 0;
+        int result = 0;
+        int i = 0;
+        while (i < noteValues.length)
+        {
+           if(sum >= noteValues[i]) {
+                value = sum / noteValues[i];
+                System.out.println("No of " + noteValues[i] + "'s" + " :" + value);
+                sum = sum % noteValues[i];
+                if (value >= 0) {
+                    result += value;
+                }
+            }
+           i++;
+        }
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        System.out.println(atm.countBanknotes(5820));
+    }
+}
